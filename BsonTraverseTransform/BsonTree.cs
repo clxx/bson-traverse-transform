114f9c5bbf5357d580b6e7d1b2b27fdc8c492876
clxx <Carsten.Lindner@gmx.net>
2022-04-25 10:48:14 +0200

    New implementation.

---
+++ BsonTraverseTransform/BsonTree.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Immutable;
+using MongoDB.Bson;
+
+namespace BsonTraverseTransform;
+
+public class BsonTree
+{
+    private static void Traverse(
+        ImmutableStack<(BsonDocument?, BsonArray?, int)> ancestors,
+        object bsonObject,
+        Action<ImmutableStack<(BsonDocument?, BsonArray?, int)>, BsonElement, BsonValue?>? preOrder,
+        Action<ImmutableStack<(BsonDocument?, BsonArray?, int)>, BsonElement, BsonValue?>? postOrder)
+    {
+        preOrder?.Invoke(ancestors, bsonObject as BsonElement? ?? default, bsonObject as BsonValue);
+        switch (bsonObject)
+        {
+            case BsonDocument bsonDocument:
+                var bsonElements = bsonDocument.ToArray();
+                for (var index = 0; index < bsonElements.Length; index++)
+                {
+                    Traverse(ancestors.Push((bsonDocument, null, index)), bsonElements[index], preOrder, postOrder);
+                }
+                break;
+            case BsonArray bsonArray:
+                var bsonValues = bsonArray.ToArray();
+                for (var index = 0; index < bsonValues.Length; index++)
+                {
+                    Traverse(ancestors.Push((null, bsonArray, index)), bsonValues[index], preOrder, postOrder);
+                }
+                break;
+            case BsonElement { Value: BsonDocument or BsonArray } bsonElement:
+                Traverse(ancestors, bsonElement.Value, preOrder, postOrder);
+                break;
+        }
+        postOrder?.Invoke(ancestors, bsonObject as BsonElement? ?? default, bsonObject as BsonValue);
+    }
+
+    public static void Traverse(
+        BsonDocument bsonDocument,
+        Action<ImmutableStack<(BsonDocument?, BsonArray?, int)>, BsonElement, BsonValue?>? preOrder,
+        Action<ImmutableStack<(BsonDocument?, BsonArray?, int)>, BsonElement, BsonValue?>? postOrder) =>
+        Traverse(
+            ImmutableStack<(BsonDocument?, BsonArray?, int)>.Empty,
+            bsonDocument,
+            preOrder,
+            postOrder);
+}
--- BsonTraverseTransform/Program.cs
+++ BsonTraverseTransform/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BsonTraverseTransform;
+using MongoDB.Bson;
+
+var bsonDocument = BsonDocument.Parse(@"{
+    ""a"": {
+        ""b"": null,
+        ""c"": ""text1"",
+        ""d"": 0,
+        ""e"": {
+            ""f"": [
+                null,
+                ""text2"",
+                1,
+                {
+                    ""g"": {
+                        ""h"": 2
+                    }
+                }
+            ]
+        },
+        ""i"": [
+            100,
+            200,
+            300
+        ]
+    }
+}");
+
+BsonTree.Traverse(
+    bsonDocument,
+    null,
+    (ancestors, bsonElement, bsonValue) =>
+    {
+        if (bsonElement.Name is "d")
+        {
+            var (document, _, index) = ancestors.Peek();
+            document?.SetElement(index, new BsonElement("zzz", bsonElement.Value));
+        }
+
+        else if (bsonElement.Name is "c")
+        {
+            var (document, _, index) = ancestors.Peek();
+            document?.RemoveAt(index);
+        }
+
+        else if (bsonValue == 200)
+        {
+            var (_, array, index) = ancestors.Peek();
+            array?.RemoveAt(index);
+        }
+    });
+
+Console.WriteLine(bsonDocument);
