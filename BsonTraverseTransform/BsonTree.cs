85cffabe52d287148d961ff9fb2dcfd21b531379
clxx <Carsten.Lindner@gmx.net>
2022-05-03 13:48:07 +0200

    Implementation.

--- BsonTraverseTransform/BsonTree.cs
+++ BsonTraverseTransform/BsonTree.cs
@@ -5,43 +5,77 @@ namespace BsonTraverseTransform;
 
 public class BsonTree
 {
-    private static void Traverse(
-        ImmutableStack<(BsonDocument?, BsonArray?, int)> ancestors,
-        object bsonObject,
-        Action<ImmutableStack<(BsonDocument?, BsonArray?, int)>, BsonElement, BsonValue?>? preOrder,
-        Action<ImmutableStack<(BsonDocument?, BsonArray?, int)>, BsonElement, BsonValue?>? postOrder)
+    private readonly Action<ImmutableStack<BsonAnchestor>, BsonElement> _preOrderElement;
+    private readonly Action<ImmutableStack<BsonAnchestor>, BsonValue> _preOrderValue;
+    private readonly Action<ImmutableStack<BsonAnchestor>, BsonElement> _postOrderElement;
+    private readonly Action<ImmutableStack<BsonAnchestor>, BsonValue> _postOrderValue;
+
+    private BsonTree(
+        Action<ImmutableStack<BsonAnchestor>, BsonElement> preOrderElement,
+        Action<ImmutableStack<BsonAnchestor>, BsonValue> preOrderValue,
+        Action<ImmutableStack<BsonAnchestor>, BsonElement> postOrderElement,
+        Action<ImmutableStack<BsonAnchestor>, BsonValue> postOrderValue)
     {
-        preOrder?.Invoke(ancestors, bsonObject as BsonElement? ?? default, bsonObject as BsonValue);
+        _preOrderElement = preOrderElement;
+        _preOrderValue = preOrderValue;
+        _postOrderElement = postOrderElement;
+        _postOrderValue = postOrderValue;
+    }
+
+    private void Traverse(ImmutableStack<BsonAnchestor> ancestors, object bsonObject)
+    {
+        switch (bsonObject)
+        {
+            case BsonElement element:
+                _preOrderElement(ancestors, element);
+                break;
+            case BsonValue value:
+                _preOrderValue(ancestors, value);
+                break;
+        }
+
         switch (bsonObject)
         {
             case BsonDocument bsonDocument:
                 var bsonElements = bsonDocument.ToArray();
                 for (var index = 0; index < bsonElements.Length; index++)
                 {
-                    Traverse(ancestors.Push((bsonDocument, null, index)), bsonElements[index], preOrder, postOrder);
+                    Traverse(ancestors.Push(new BsonDocumentAnchestor(bsonDocument, index)), bsonElements[index]);
                 }
                 break;
             case BsonArray bsonArray:
                 var bsonValues = bsonArray.ToArray();
                 for (var index = 0; index < bsonValues.Length; index++)
                 {
-                    Traverse(ancestors.Push((null, bsonArray, index)), bsonValues[index], preOrder, postOrder);
+                    Traverse(ancestors.Push(new BsonArrayAnchestor(bsonArray, index)), bsonValues[index]);
                 }
                 break;
             case BsonElement { Value: BsonDocument or BsonArray } bsonElement:
-                Traverse(ancestors, bsonElement.Value, preOrder, postOrder);
+                Traverse(ancestors, bsonElement.Value);
                 break;
         }
-        postOrder?.Invoke(ancestors, bsonObject as BsonElement? ?? default, bsonObject as BsonValue);
+
+        switch (bsonObject)
+        {
+            case BsonElement element:
+                _postOrderElement(ancestors, element);
+                break;
+            case BsonValue value:
+                _postOrderValue(ancestors, value);
+                break;
+        }
     }
 
     public static void Traverse(
         BsonDocument bsonDocument,
-        Action<ImmutableStack<(BsonDocument?, BsonArray?, int)>, BsonElement, BsonValue?>? preOrder,
-        Action<ImmutableStack<(BsonDocument?, BsonArray?, int)>, BsonElement, BsonValue?>? postOrder) =>
-        Traverse(
-            ImmutableStack<(BsonDocument?, BsonArray?, int)>.Empty,
-            bsonDocument,
-            preOrder,
-            postOrder);
+        Action<ImmutableStack<BsonAnchestor>, BsonElement>? preOrderElement = null,
+        Action<ImmutableStack<BsonAnchestor>, BsonValue>? preOrderValue = null,
+        Action<ImmutableStack<BsonAnchestor>, BsonElement>? postOrderElement = null,
+        Action<ImmutableStack<BsonAnchestor>, BsonValue>? postOrderValue = null) =>
+        new BsonTree(
+                preOrderElement ?? ((_, _) => { }),
+                preOrderValue ?? ((_, _) => { }),
+                postOrderElement ?? ((_, _) => { }),
+                postOrderValue ?? ((_, _) => { }))
+            .Traverse(ImmutableStack<BsonAnchestor>.Empty, bsonDocument);
 }
